1783b1292c0b3305e167f8799313769b8493ff41
x13nnn <jungleissohard@gmail.com>
2025-03-25 21:08:37 +0800

    Updates

---
+++ static/js/952.0f9cffff.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_static_site=self.webpackChunkmy_static_site||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.0f9cffff.chunk.js.map
